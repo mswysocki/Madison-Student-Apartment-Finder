7195c3afd24d3361e61733f38fd7251489aa2171
Cory Romdenne <cromdenne11@gmail.com>
2012-03-19 22:27:17 -0500

    Moved forms from js to html, and made js much cleaner.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,99 +1,161 @@
 // JavaScript Document
 
-function displayPriceCriteria()
+function showPrice(id)
 {
-	return "<form method=\"GET\"><input type=\"text\" name=\"Price\" value=\"Maximum Price\"/></form>";
-}
-
-function displayLocationCriteria()
-{
-	return "<form method=\"GET\"><input type=\"text\" name=\"Location\" value=\"Address\"/></form>";
-}
-
-function displayTypeCriteria()
-{
-	return "<form method=\"GET\"><input type=\"checkbox\" name=\"Type\" value=\"Apartment\">Apartment</input><br /><input type=\"checkbox\" name=\"Type\" value=\"House\">House</input><br /><input type=\"checkbox\" name=\"Type\" value=\"Townhouse\">Townhouse</input></form>";
-}
-
-function displayResDetailsCriteria()
-{
-	return "<form method=\"GET\">Number of Bedrooms<br /><input type=\"text\" name=\"Bedrooms\" value=\"Maximum 8\" /><br /><br />Number of Bathrooms<br /><input type=\"text\" name=\"Bathrooms\" value=\"Maximum 4\" /></form>";
-}
-
-function displayLeaseDetails()
-{
-	return "<form method=\"GET\">Start Date:<br /><input type=\"radio\" name=\"Start\" value\"Fall\">Fall</input><br /><input type=\"radio\" name=\"Start\" value\"Winter\">Winter</input></form><br /><form method=\"GET\">End Date:<br /><input type=\"radio\" name=\"End\" value\"Winter\">Winter</input><br /><input type=\"radio\" name=\"End\" value\"Spring\">Spring</input><br /><input type=\"radio\" name=\"End\" value\"Summer\">Summer</input></form><br /><form method=\"GET\">Length:<br /><input type=\"radio\" name=\"Length\" value\"6 Months\">6 Months</input><br /><input type=\"radio\" name=\"Length\" value\"9 Months\">9 Months</input><br /><input type=\"radio\" name=\"Length\" value\"12 Months\">12 Months</input></form>";
-}
-
-function displayUtilityDetails()
-{
-	return "<form method=\"GET\"><input type=\"checkbox\" name=\"Utility\" value=\"Heat\">Heat</input><br /><input type=\"checkbox\" name=\"Utility\" value=\"Water\">Water</input><br /><input type=\"checkbox\" name=\"Utility\" value=\"Electricity\">Electricity</input><br /><input type=\"checkbox\" name=\"Utility\" value=\"Gas\">Gas</input><br /><input type=\"checkbox\" name=\"Utility\" value=\"Garbage\">Garbage Collection</input></form>";
-}
-
-function displayAccomodationsDetails()
-{
-	return "<form method=\"GET\"><input type=\"checkbox\" name=\"Accomodation\" value=\"Parking\">Parking</input><br /><input type=\"checkbox\" name=\"Accomodation\" value=\"Laundry\">Laundry</input><br /><input type=\"checkbox\" name=\"Accomodation\" value=\"Furnishings\">Furnishings</input><br /><input type=\"checkbox\" name=\"Accomodation\" value=\"Yard\">Yard Maintainance & Snow Removal (houses only)</input></form>";
-}
-
-function displayRestrictionsDetails()
-{
-	return "<form method=\"GET\"><input type=\"checkbox\" name=\"Restriction\" value=\"Smoking\">Smoking Allowed</input><br /><input type=\"checkbox\" name=\"Restriction\" value=\"Pets\">Pets Allowed</input></form>";
-}
-
-function setUnclicked(id)
-{
-	var html = "<p onclick=\"setClicked('"+id+"')\">"+id+"</p>";
+	var tmpID = id;
 	
-	switch(id)
+	if (id == 'Price_Ops_Invis')
 	{
-		case "Type_of_Residence":
-			html = "<p onclick=\"setClicked('Type_of_Residence')\">Type of Residence</p>";
-			break;
-		case "Residence_Details":
-			html = "<p onclick=\"setClicked('Residence_Details')\">Residence Details</p>";
-			break;
-		case "Lease_Details":
-			html = "<p onclick=\"setClicked('Lease_Details')\">Lease Details</p>";
-			break;
+		id = 'Price_Ops_Vis';
+		document.getElementById('Price').id = "Price_All";
+	}
+	else
+	{
+		id = 'Price_Ops_Invis';
+		document.getElementById('Price_All').id = "Price";
 	}
 	
-	document.getElementById(id).innerHTML=html;
+	document.getElementById('PriceHandler').innerHTML = "<p onclick=\"showPrice('"+id+"')\">Price</p>";
+	
+	document.getElementById(tmpID).id = id;
 }
 
-function setClicked(id)
+function showLocation(id)
 {
-	var html = "<div id=\"Criteria_Clicked\"><p onclick=\"setUnclicked('"+id+"')\">"+id+"</p>";
+	var tmpID = id;
 	
-	switch(id){
-		case "Price":
-			html += displayPriceCriteria();
-			break;
-		case "Location":
-			html += displayLocationCriteria();
-			break;
-		case "Type_of_Residence": 
-			html = "<div id=\"Criteria_Clicked\"><p onclick=\"setUnclicked('Type_of_Residence')\">Type of Residence</p>";
-			html += displayTypeCriteria();
-			break;
-		case "Residence_Details": 
-			html = "<div id=\"Criteria_Clicked\"><p onclick=\"setUnclicked('Residence_Details')\">Residence Details</p>";
-			html += displayResDetailsCriteria();
-			break;
-		case "Lease_Details": 
-			html = "<div id=\"Criteria_Clicked\"><p onclick=\"setUnclicked('Lease_Details')\">Lease Details</p>";
-			html += displayLeaseDetails();
-			break;
-		case "Utilities":
-			html += displayUtilityDetails();
-			break;
-		case "Accomodations":
-			html += displayAccomodationsDetails();
-			break;
-		case "Restrictions":
-			html += displayRestrictionsDetails();
-			break;
+	if (id == 'Location_Ops_Invis')
+	{
+		id = 'Location_Ops_Vis';
+		document.getElementById('Location').id = "Location_All";
+	}
+	else
+	{
+		id = 'Location_Ops_Invis';
+		document.getElementById('Location_All').id = "Location";
 	}
 	
-	html += "</div>"
-	document.getElementById(id).innerHTML=html;
+	document.getElementById('LocationHandler').innerHTML = "<p onclick=\"showLocation('"+id+"')\">Location</p>";
+	
+	document.getElementById(tmpID).id = id;
+}
+
+function showType(id)
+{
+	var tmpID = id;
+	
+	if (id == 'Type_Ops_Invis')
+	{
+		id = 'Type_Ops_Vis';
+		document.getElementById('Type_of_Res').id = "Type_of_Res_All";
+	}
+	else
+	{
+		id = 'Type_Ops_Invis';
+		document.getElementById('Type_of_Res_All').id = "Type_of_Res";
+	}
+	
+	document.getElementById('TypeHandler').innerHTML = "<p onclick=\"showType('"+id+"')\">Type of Residence</p>";
+	
+	document.getElementById(tmpID).id = id;
+}
+
+function showDetails(id)
+{
+	var tmpID = id;
+	
+	if (id == 'Details_Ops_Invis')
+	{
+		id = 'Details_Ops_Vis';
+		document.getElementById('Res_Details').id = "Res_Details_All";
+	}
+	else
+	{
+		id = 'Details_Ops_Invis';
+		document.getElementById('Res_Details_All').id = "Res_Details";
+	}
+	
+	document.getElementById('DetailsHandler').innerHTML = "<p onclick=\"showDetails('"+id+"')\">Residence Details</p>";
+	
+	document.getElementById(tmpID).id = id;
+}
+
+function showLease(id)
+{
+	var tmpID = id;
+	
+	if (id == 'Lease_Ops_Invis')
+	{
+		id = 'Lease_Ops_Vis';
+		document.getElementById('Lease_Details').id = "Lease_Details_All";
+	}
+	else
+	{
+		id = 'Lease_Ops_Invis';
+		document.getElementById('Lease_Details_All').id = "Lease_Details";
+	}
+	
+	document.getElementById('LeaseHandler').innerHTML = "<p onclick=\"showLease('"+id+"')\">Lease Details</p>";
+	
+	document.getElementById(tmpID).id = id;
+}
+
+function showUtils(id)
+{
+	var tmpID = id;
+	
+	if (id == 'Utils_Ops_Invis')
+	{
+		id = 'Utils_Ops_Vis';
+		document.getElementById('Utilities').id = "Utils_All";
+	}
+	else
+	{
+		id = 'Utils_Ops_Invis';
+		document.getElementById('Utils_All').id = "Utilities";
+	}
+	
+	document.getElementById('UtilsHandler').innerHTML = "<p onclick=\"showUtils('"+id+"')\">Utilities</p>";
+	
+	document.getElementById(tmpID).id = id;
+}
+
+function showAccoms(id)
+{
+	var tmpID = id;
+	
+	if (id == 'Accoms_Ops_Invis')
+	{
+		id = 'Accoms_Ops_Vis';
+		document.getElementById('Accomodations').id = "Accoms_All";
+	}
+	else
+	{
+		id = 'Accoms_Ops_Invis';
+		document.getElementById('Accoms_All').id = "Accomodations";
+	}
+	
+	document.getElementById('AccomsHandler').innerHTML = "<p onclick=\"showAccoms('"+id+"')\">Accomodations</p>";
+	
+	document.getElementById(tmpID).id = id;
+}
+
+function showRestricts(id)
+{
+	var tmpID = id;
+	
+	if (id == 'Restricts_Ops_Invis')
+	{
+		id = 'Restricts_Ops_Vis';
+		document.getElementById('Restrictions').id = "Restricts_All";
+	}
+	else
+	{
+		id = 'Restricts_Ops_Invis';
+		document.getElementById('Restricts_All').id = "Restrictions";
+	}
+	
+	document.getElementById('RestrictsHandler').innerHTML = "<p onclick=\"showRestricts('"+id+"')\">Restrictions</p>";
+	
+	document.getElementById(tmpID).id = id;
 }
