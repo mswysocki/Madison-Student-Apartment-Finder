c8597f103e0ced25e81cac8dc7b2d1419d2dc1ea
Adam Eggum <aeggum12@gmail.com>
2012-03-20 22:38:49 -0500

    Added validations and default values. We will need to agree on 'max' values for various things.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,25 @@
 class List < ActiveRecord::Base
+  attr_accessible :Address, :City, :State, :Zip, :Region, :Bedrooms, 
+    :Bathrooms, :Rent, :SquareFeet, :Parking, :Smoking, :Pets, :Heat, 
+    :Electric, :Flags
+  
+  #requires that these three are filled in + add some validations
+  validates :Address,   :presence => true,
+                        :length   => { :maximum => 50 }, 
+                        :uniqueness => { :case_sensitive => false }
+  validates :Bedrooms,  :presence => true,
+                        :format => { :with => /^\d+??(?:\.\d{0,2})?$/ }, 
+                        :numericality => {:greater_than => 0, :less_than => 25}
+  validates :Rent,      :presence => true,
+                        :format => { :with => /^\d+??(?:\.\d{0,2})?$/ }, 
+                        :numericality => {:greater_than => 1, :less_than => 25000}
+  
+  #sets default values for the db entry when the listing is saved
+  before_save :default_values
+  def default_values
+    self.City ||= "Madison"
+    self.State ||= "Wisconsin"
+    self.Flags ||= 0
+  end
+
 end
