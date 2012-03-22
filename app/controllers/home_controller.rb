71a15f28467f8491ff5c3cf0d1e4cfae5e4d17d3
Adam Eggum <aeggum12@gmail.com>
2012-03-22 17:49:13 -0500

    Getting Search onto home page.  Still a work in progress.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
+    @search = List.search(params[:search])
+    @test = params[:search]
+    @lists = @search.all
   end
 
+  private 
+  
+  def get_variable
+    @list = List.all
+  end
 end
