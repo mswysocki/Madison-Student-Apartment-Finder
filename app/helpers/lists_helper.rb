9ba1ab78a0864e25c8a3caffe3a167dc9b6cc127
Adam Eggum <aeggum12@gmail.com>
2012-03-22 01:53:42 -0500

    Added some basic logic to not show null fields on the show page.

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,12 @@
 module ListsHelper
+  
+  #returns if an element is nil. 
+  #Not useful in current state, as it is not even any shorter
+  def is_nil(element)
+    if element.nil?
+      return true
+    else 
+      return false
+    end
+  end
 end
