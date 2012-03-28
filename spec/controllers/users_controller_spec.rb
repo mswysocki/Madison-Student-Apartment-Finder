eeb2cece726f0b3136ccf5a5664627081321e19e
Adam Eggum <aeggum12@gmail.com>
2012-03-28 02:44:48 -0500

    User/Security tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,45 @@ require 'spec_helper'
 describe UsersController do
   render_views
   
+  describe "GET 'index'" do
+
+    describe "for non-signed-in users" do
+      it "should deny access" do
+        get :index
+        response.should redirect_to(signin_path)
+        flash[:notice].should =~ /sign in/i
+      end
+    end
+
+    describe "for signed-in users" do
+
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        second = Factory(:user, :Name => "Bob", :Email => "another@example.com")
+        third  = Factory(:user, :Name => "Ben", :Email => "another@example.net")
+
+        @users = [@user, second, third]
+      end
+
+      it "should be successful" do
+        get :index
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        get :index
+        response.should have_selector("title", :content => "All users")
+      end
+
+      it "should have an element for each user" do
+        get :index
+        @users.each do |user|
+          response.should have_selector("li", :content => user.Name)
+        end
+      end
+    end
+  end
+  
   describe "GET 'new'" do
     it "should be successful" do
       get :new
@@ -42,6 +81,86 @@ describe UsersController do
     end
   end
   
+  describe "GET 'edit'" do
+    
+    before(:each) do
+      @user = Factory(:user) 
+      test_sign_in(@user)
+    end
+    
+    it "should be successful" do
+      get :edit, :id => @user
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get :edit, :id => @user
+      response.should have_selector("title", :content => "Edit user")
+    end
+  end
+  
+  describe "PUT 'update'" do 
+    before(:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+    
+    describe "failure" do
+      before(:each) do
+        @attr = { :Email => "", :Name => "", :password => "", :password_confirmation => ""}
+      end
+      
+      it "should render the 'edit page'" do
+        put :update, :id => @user, :user => @attr
+        response.should have_selector('title', :content => "Edit user")
+      end
+      
+      it "should have the right title" do
+        put :update, :id => @user, :user => @attr
+        response.should have_selector("title", :content => "Edit user")
+      end
+    end
+    
+    describe "success" do
+
+      before(:each) do
+        @attr = { :Name => "Example Name", :Email => "email@mail.com",
+                  :password => "password", :password_confirmation => "password" }
+      end
+
+      it "should change the user's attributes" do
+        put :update, :id => @user, :user => @attr
+        @user.reload
+        @user.Name.should  == @attr[:Name]
+        @user.Email.should == @attr[:Email]
+      end
+
+      it "should redirect to the user show page" do
+        put :update, :id => @user, :user => @attr
+        response.should redirect_to(user_path(@user))
+      end
+    end
+  end
+  
+  describe "authentication of edit/update pages" do
+
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    describe "for non-signed-in users" do
+
+      it "should deny access to 'edit'" do
+        get :edit, :id => @user
+        response.should redirect_to(signin_path)
+      end
+
+      it "should deny access to 'update'" do
+        put :update, :id => @user, :user => {}
+        response.should redirect_to(signin_path)
+      end
+    end
+  end
   
   describe "POST 'create'" do
     describe "failure" do
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -56,8 +56,70 @@ describe "Users" do
         click_button
         controller.should be_signed_in
         click_link "Sign out"
-        controller.should_not_be_signed_in
+        controller.should_not be_signed_in
       end
     end
   end
+  
+  describe "admin attribute" do
+
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+
+    it "should respond to admin" do
+      @user.should respond_to(:admin)
+    end
+
+    it "should not be an admin by default" do
+      @user.should_not be_admin
+    end
+
+    it "should be convertible to an admin" do
+      @user.toggle!(:admin)
+      @user.should be_admin
+    end
+  end
+  
+  describe "DELETE 'destroy'" do
+
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    describe "as a non-signed-in user" do
+      it "should deny access" do
+        delete :destroy, :id => @user
+        response.should redirect_to(signin_path)
+      end
+    end
+
+    describe "as a non-admin user" do
+      it "should protect the page" do
+        test_sign_in(@user)
+        delete :destroy, :id => @user
+        response.should redirect_to(root_path)
+      end
+    end
+
+    describe "as an admin user" do
+
+      before(:each) do
+        admin = Factory(:user, :mail => "admin@example.com", :admin => true)
+        test_sign_in(admin)
+      end
+
+      it "should destroy the user" do
+        lambda do
+          delete :destroy, :id => @user
+        end.should change(User, :count).by(-1)
+      end
+
+      it "should redirect to the users page" do
+        delete :destroy, :id => @user
+        response.should redirect_to(users_path)
+      end
+    end
+  end
+  
 end
