304d02e07f3966d909f313f6fe3fdc932aa453b1
Adam Eggum <aeggum12@gmail.com>
2012-03-27 22:40:09 -0500

    Made the title code a little bit less, dependent, with a default title at the start.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
   #return a title on a per-page basis
-    #this could be used to make our titles in the future..with an append
   def full_title
     base_title = "Madison Student Housing Finder";
     if @title.nil?
