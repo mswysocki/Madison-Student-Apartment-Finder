5a262cf4584a5ae62010fb30905146ee619f7842
Adam Eggum <aeggum12@gmail.com>
2012-03-28 02:15:34 -0500

    More testing for the user, controller, and list.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -92,6 +92,11 @@ describe UsersController do
         flash[:success].should =~ /Welcome to the Madison Student Housing Finder!/i
       end
       
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
+      
     end
     
     
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -27,6 +27,38 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "New Listing")
   end
   
+  describe "when not signed in" do
+    it "should have a signin link" do
+      visit home_page_path
+      response.should have_selector("a", :href => signin_path,
+                                         :content => "Sign in")
+    end
+  end
   
+  describe "when signed in" do
+    
+    #sign in beforehand
+    before(:each) do
+      @user = Factory(:user)
+      visit signin_path
+      fill_in :Email,       :with => @user.Email
+      fill_in :password,    :with => @user.password
+      click_button
+    end
+    
+    
+    it "should have a signout link" do
+      visit home_page_path
+      response.should have_selector("a", :href => signout_path,
+                                         :content => "Sign out")
+    end
+    
+    it "should have a profile link" do
+      visit home_page_path
+      response.should have_selector("a", :href => user_path(@user),
+                                         :content => "Profile")
+    end
+    
+  end
   
 end
