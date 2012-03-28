ead778127c5579bb1bacb5dfcf83c61e45b1050e
Adam Eggum <aeggum12@gmail.com>
2012-03-28 00:58:30 -0500

    More tests for the Users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -15,6 +15,35 @@ describe UsersController do
     end
   end
   
+  describe "GET 'show'" do
+    
+    before(:each) do
+      @user = Factory(:user) 
+    end
+    
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
+    end
+    
+    it  "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+    
+    it "should have the right title" do
+      get :show, :id => @user 
+      response.should have_selector("title", :content => "Madison Student Housing Finder | " << @user.Name)
+    end
+    
+    it "should contain the user's name" do
+      get :show, :id => @user
+      response.should have_selector("h3", :content => @user.Name)
+    end
+    
+    
+  end
+  
   
   
 end
