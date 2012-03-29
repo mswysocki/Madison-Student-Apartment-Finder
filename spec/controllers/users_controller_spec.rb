010bb1589d0e043faa3eadfddfb939ab63ad3b70
Adam Eggum <aeggum12@gmail.com>
2012-03-29 17:11:20 -0500

    Fixed bugs that I had made when I made the tests before.  If you run the tests now, all of them should pass.  You will need to do a bundle install and then 'rspec spec/' to run the tests.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -215,12 +215,54 @@ describe UsersController do
         post :create, :user => @attr
         controller.should be_signed_in
       end
-      
     end
-    
-    
-    
   end
+  
+  
+  
+  describe "DELETE 'destroy'" do
+
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    describe "as a non-signed-in user" do
+      it "should deny access" do
+        delete :destroy, :id => @user
+        response.should redirect_to(signin_path)
+      end
+    end
+
+    describe "as a non-admin user" do
+      it "should protect the page" do
+        test_sign_in(@user)
+        delete :destroy, :id => @user
+        response.should redirect_to(home_page_path)
+      end
+    end
+      
+
+    describe "as an admin user" do
+
+      before(:each) do
+        admin = Factory(:user, :Email => "admin@example.com", :admin => true)
+        test_sign_in(admin)
+      end
+
+      it "should destroy the user" do
+        lambda do
+          delete :destroy, :id => @user
+        end.should change(User, :count).by(-1)
+      end
+
+      it "should redirect to the users page" do
+        delete :destroy, :id => @user
+        response.should redirect_to(users_path)
+      end
+    end
+  end
+  
+  
   
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -126,7 +126,26 @@ describe User do
     
   end
   
-  
+  describe "admin attribute" do
+
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+
+    it "should respond to admin" do
+      puts @user.admin
+      @user.should respond_to(:admin)
+    end
+
+    it "should not be an admin by default" do
+      @user.should_not be_admin
+    end
+
+    it "should be convertible to an admin" do
+      @user.toggle!(:admin)
+      @user.should be_admin
+    end
+  end
   
   
   
