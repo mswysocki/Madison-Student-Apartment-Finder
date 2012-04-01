c7c4341f38948f7b1b985474f55344e986cc73e3
Adam Eggum <aeggum12@gmail.com>
2012-03-31 22:08:54 -0500

    Added sorting helper method for the admin page (can be used in any controller, or view), however.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,9 +12,9 @@ module ApplicationHelper
   #Unused helper function
   def sortable(column, title = nil)
     title ||= column.titleize
-    #css_class = column == sort_column ? "current #{sort_direction}" : nil
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, :sort => column, :direction => direction
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
   end
   
   
