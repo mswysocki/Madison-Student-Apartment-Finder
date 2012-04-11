10540e6b8d90fea2f1334abe96434b9b5279ae0d
Guilherme S. G. Baptista <guisgb13@gmail.com>
2012-04-11 18:22:32 -0300

    Started changing CSS of Results Display to look more like the sample UI we had designed before.

--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,6 @@ ActiveRecord::Schema.define(:version => 20120401161635) do
     t.boolean  "admin",              :default => false
   end
 
-  add_index "users", ["Email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["Email"], :name => "index_users_on_Email", :unique => true
 
 end
