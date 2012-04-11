3e00e99119f4d1b5a0f2dc9324835f2b92273df6
Adam Eggum <aeggum12@gmail.com>
2012-04-11 01:02:20 -0500

    Added many tests for the 'show' action of the lists controller.  More are on the way currently.

--- spec/factories.rb
+++ spec/factories.rb
@@ -15,17 +15,27 @@ Factory.define :list do |list|
   list.bathrooms                  2
   list.rent                       1800
   list.squarefeet                 2000
-  list.smoking                    true
-  list.heat                       true
-  list.length                     12
-  list.electric                   false
-  list.flags                      0
-  list.gas                        false
-  list.garbagecollection          true
-  list.furnished                  true
-  list.laundry                    true
-  list.parking                    false
-  list.bedrooms                   5 
-  list.pets                       false
-  list.ltype                      true
+  list.smoking                    nil
+  list.heat                       nil
+  list.length                     nil
+  list.electric                   nil
+  list.flags                      nil
+  list.gas                        nil
+  list.garbagecollection          nil
+  list.furnished                  nil
+  list.laundry                    nil
+  list.parking                    nil
+  list.bedrooms                   5
+  list.pets                       nil
+  list.ltype                      nil
+end
+
+Factory.define :list1 do |list| 
+  list.address                    "Sample Default Address"
+  list.city                       "Madison"
+  list.state                      "Wisconsin"
+  list.zip                        53726
+  list.rent                       2000
+  list.bedrooms                   6
+  list.bathrooms                  2
 end
