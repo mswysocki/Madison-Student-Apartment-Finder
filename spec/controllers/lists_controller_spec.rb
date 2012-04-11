d88dec48e38cb6987507ed0e125860b482a0c926
Adam Eggum <aeggum12@gmail.com>
2012-04-11 02:08:55 -0500

    More List tests.

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -170,10 +170,54 @@ describe ListsController do
       get 'new' 
       response.should be_success
     end
+    
+    it "should have editable address field" do
+      get :new
+      response.should have_selector("div", :id => "NewAddress", :class => "field")
+    end
+    
+    it "should not have non-editable address field" do
+      get :new
+      response.should_not have_selector("div", :id => "NoEdit_NewAddress", :class => "field")
+    end
+    
+    it "should have editable zip field" do
+      get :new
+      response.should have_selector("div", :id => "NewZip", :class => "field")
+    end
+    
+    it "should not have non-editable zip field" do
+      get :new
+      response.should_not have_selector("div", :id => "NoEdit_NewZip", :class => "field")
+    end
+    
+    it "should have editable bedrooms field" do
+      get :new
+      response.should have_selector("div", :id => "NewBedrooms", :class => "field")
+    end
+    
+    it "should not have non-editable bedrooms field" do
+      get :new
+      response.should_not have_selector("div", :id => "NoEdit_NewBedrooms", :class => "field")
+    end
+    
+    it "should have editable bathooms field" do
+      get :new
+      response.should have_selector("div", :id => "NewBathrooms", :class => "field")
+    end
+    
+    it "should not have non-editable bathrooms field" do
+      get :new
+      response.should_not have_selector("div", :id => "NoEdit_NewBathrooms", :class => "field")
+    end
   end
   
   
   describe "GET 'edit'" do
+    before(:each) do
+      @listing = Factory(:list)
+    end
+    
     it "should have the right title" do
       listing = List.create!(:address => "1022 W Johnson St. #507", :city => "Madison", :state => "Wisconsin", :zip => 53715, :region => 2, :bedrooms => 3, :bathrooms => 2, :rent => 2660, :parking => true, :smoking => false, :pets => false, :heat => false, :flags => 10, :gas => true, :garbagecollection => true, :length => 12, :furnished => false, :laundry => true, :ltype => true)
       get 'edit', :id => listing.id
@@ -185,6 +229,46 @@ describe ListsController do
       get 'edit', :id => listing.id
       response.should be_success
     end
+    
+    it "should not have non editable address field" do
+      get :edit, :id => @listing
+      response.should_not have_selector("div", :id => "NewAddress", :class => "field")
+    end
+    
+    it "should have non-editable address field" do
+      get :edit, :id => @listing
+      response.should have_selector("div", :id => "NoEdit_NewAddress", :class => "field")
+    end
+    
+    it "should not have editable zip field" do
+      get :edit, :id => @listing
+      response.should_not have_selector("div", :id => "NewZip", :class => "field")
+    end
+    
+    it "should have non-editable zip field" do
+      get :edit, :id => @listing
+      response.should have_selector("div", :id => "NoEdit_NewZip", :class => "field")
+    end
+    
+    it "should not have editable bedrooms field" do
+      get :edit, :id => @listing
+      response.should_not have_selector("div", :id => "NewBedrooms", :class => "field")
+    end
+    
+    it "should have non-editable bedrooms field" do
+      get :edit, :id => @listing
+      response.should have_selector("div", :id => "NoEdit_NewBedrooms", :class => "field")
+    end
+    
+    it "should not have editable bathooms field" do
+      get :edit, :id => @listing
+      response.should_not have_selector("div", :id => "NewBathrooms", :class => "field")
+    end
+    
+    it "should have non-editable bathrooms field" do
+      get :edit, :id => @listing
+      response.should have_selector("div", :id => "NoEdit_NewBathrooms", :class => "field")
+    end
   end
   
   
@@ -193,21 +277,113 @@ describe ListsController do
   
   # attempt to do the create testing but I don't know rspec enough to do so at this point
   describe "POST 'create'" do
-    it "should be successful" do
-      listing = List.create!(:address => "1022 W Johnson St. #507", :city => "Madison", :state => "Wisconsin", :zip => 53715, :region => 2, :bedrooms => 3, :bathrooms => 2, :rent => 2660, :parking => true, :smoking => false, :pets => false, :heat => false, :flags => 10, :gas => true, :garbagecollection => true, :length => 12, :furnished => false, :laundry => true, :ltype => true)
-      post 'create', :params => listing
-      response.should be_success
+    
+    describe "failure" do
+      before(:each) do
+        @attr = { :address => "", :bathrooms => -1, :bedrooms => -1, :zip => 0}
+      end
+      
+      it "should not create a new listing" do
+        lambda do
+          post :create, :list => @attr
+        end.should_not change(List, :count)
+      end
+      
+      it "should have the right title" do
+        post :create, :list => @attr
+        response.should have_selector("title", :content => "New Listing")
+      end
+      
+      it "should render the 'new' page" do
+        post :create, :list => @attr
+        response.should render_template('new')
+      end
     end
+    
+    describe "success" do
+      before(:each) do
+        @attr = { :address => "SAMPLE CREATED LISTING", :city => "Madison", :state => "Wisconsin", :zip => 53726, :region => 3, :bedrooms => 3, :bathrooms => 1, :rent => 1600, :parking => false, :smoking => false, :pets => false, :heat => false, :flags => 10, :gas => true, :garbagecollection => true, :ltype => true, :length => 12, :furnished => false, :laundry => true }
+      end
+      
+      #These two tests fail because of the recaptcha stuff.
+      it "should create a new listing" do
+        lambda do
+          post :create, :list => @attr
+        end.should change(List, :count).by(1)
+      end
+      
+      it "should redirect to the listing show page" do
+        post :create, :list => @attr
+        response.should redirect_to(list_path(assigns(:list)))
+      end
+    end
+    
   end
   
   #Similar failings to the create code...I don't know how to pass what I need into the controller
   describe "PUT 'update'" do
-    it "should be successful" do
-      listing = List.create!(:address => "1022 W Johnson St. #507", :city => "Madison", :state => "Wisconsin", :zip => 53715, :region => 2, :bedrooms => 3, :bathrooms => 2, :rent => 2660, :parking => true, :smoking => false, :pets => false, :heat => false, :flags => 10, :gas => true, :garbagecollection => true, :length => 12, :furnished => false, :laundry => true, :ltype => true)
-      put 'update', :id => listing.id, :params => listing
+    before(:each) do
+      @listing = Factory(:list)
+    end
+    
+    it "doing nothing of importance should be successful" do
+      put 'update', :id => @listing, :list => @listing
       response.should be_success
+    end
+    
+    describe "failure" do
+      before(:each) do
+        @attr = { :address => "", :zip => 0, :bedrooms => -1, :bathrooms => -1 }
+      end
+      
+      it "should render the 'edit page'" do
+        put :update, :id => @listing, :list => @attr
+        response.should have_selector('title', :content => "Update Listing")
+      end
+      
+      it "should have the right title" do
+        put :update, :id => @listing, :list => @attr
+        response.should have_selector("title", :content => "Update Listing")
+      end
+    end
+    
+    describe "success" do
+      #Currently unable to do the full testing here, as something is causing an error
+      
+      before(:each) do
+        @attr = { :address => "TEST ONLY", :city => "Madison", :state => "Wisconsin", :zip => 53726, :region => 3, :bedrooms => 3, :bathrooms => 1, :rent => 1600, :parking => false, :smoking => false, :pets => false, :heat => false, :flags => 10, :gas => true, :garbagecollection => true, :ltype => true, :length => 12, :furnished => false, :laundry => true }
+      end
+      
+      #it "should change the listings attributes" do
+      #  put :update, :id => @listing, :list => @attr
+      #  @listing.reload
+      #  @listing.address.should == @attr[:address]
+      #  @listing.zip.should == @attr[:zip]
+      #  @listing.bedrooms.should == @attr[:bedrooms]
+      #end
+      
+      it "should redirect to the listing show page" do
+        put :update, :id => @listing, :list => @attr
+        response.should have_selector("title", :content => "Madison Student Housing Finder |")
+      end
     end
   end
   
   
+  describe "DELETE 'destroy'" do
+    before(:each) do
+      @listing = Factory(:list)
+    end
+    
+    it "should destroy the listing" do
+      lambda do
+        delete :destroy, :id => @listing
+      end.should change(List, :count).by(-1)
+    end
+    
+    it "should redirect to the lists page (empty search for now)" do
+      delete :destroy, :id => @listing
+      response.should redirect_to(lists_path)
+    end
+  end
 end
