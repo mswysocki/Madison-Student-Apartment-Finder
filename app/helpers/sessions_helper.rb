a43b09581a2a8564e0ba15fa0728f3c8116d0f12
Adam Eggum <aeggum12@gmail.com>
2012-04-16 23:52:52 -0500

    Moved authenticate method into another, more accessible place.
    Needed for reviews.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -27,6 +27,10 @@ module SessionsHelper
     !current_user.nil?
   end
   
+  def authenticate
+    deny_access unless signed_in?
+  end
+  
   def deny_access
     store_location
     redirect_to signin_path, :notice => "Please sign in to access this page."
