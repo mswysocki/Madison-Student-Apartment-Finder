0b4ad54a01bbbf8ef2fe7420d69898ee6d62df06
Adam Eggum <aeggum12@gmail.com>
2012-04-17 00:35:32 -0500

    Added a helper method to print the name of the user who posted the comment.

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,12 @@
 module ReviewsHelper
+  
+  #gets the user associated with the given user id
+  def get_user(user_id)
+    if user_id.nil? 
+      return
+    end
+    
+    user = User.find(user_id)
+    return user
+  end 
 end
