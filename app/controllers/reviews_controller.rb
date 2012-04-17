82133a2ce2416b9d7901176e31782f769038e3c1
Adam Eggum <aeggum12@gmail.com>
2012-04-16 23:52:05 -0500

    Changes to the reviews controller to get the basics working.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,24 +1,37 @@
 class ReviewsController < ApplicationController
+  before_filter :authenticate, :only => [:create, :destroy]
   
-  def new
-    @review = Review.new
-    @title = "New Review"
+  
+  def index
+    
   end
   
-  
+ 
+  #TODO: Fix what the redirects are in these cases
   def create
-	@review = Review.new(params[:list])
-	
-	respond_to do |format|
-      if (@review.valid?)
-        @review.save
-        format.html { redirect_to(@list, :notice => 'Review was successfully created.') }
-        format.xml  { render :xml => @list, :status => :created, :location => @review }
-      else
-        puts;
-      end
+    #listing = get_listing()
+    @listing = List.find(params[:list_id])
+    @review = @listing.reviews.build(params[:review])
+    if (@review.save)
+      flash[:success] = "Review created!"
+      redirect_to @listing #for now..will change
+    else 
+      flash[:error] = "You suck"
+      redirect_to @listing
+      #render 'lists/1/show'
     end
   end
+	#@review = Review.new(params[:list])
+	
+#	respond_to do |format|
+    #  if (@review.valid?)
+   #     @review.save
+  #      format.html { redirect_to(@list, :notice => 'Review was successfully created.') }
+ #       format.xml  { render :xml => @list, :status => :created, :location => @review }
+ #     else
+  #      puts;
+    #  end
+ #   end
 
   def update
 	@review = Review.find(params[:id])
