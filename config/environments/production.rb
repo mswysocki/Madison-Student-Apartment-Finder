d93324a2a979a6aa1f47a11f35341b272b0c2fff
Adam Eggum <aeggum12@gmail.com>
2012-04-22 00:19:25 -0500

    (Hopefully fixed crash on heroku)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,5 +48,6 @@ MadisonStudentApartmentFinder::Application.configure do
   config.active_support.deprecation = :notify
   
   config.action_mailer.default_url_options = { :host => "msaf.heroku.com" }
-  
+  config.action_mailer.delivery_method = :smtp
+
 end
