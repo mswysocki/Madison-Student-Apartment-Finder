f5639bdc923b21e02eb1e979c43a608c900ff463
Adam Eggum <aeggum12@gmail.com>
2012-04-23 00:43:05 -0500

    Fixed the latest issue, I believe.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id          :integer         not null, primary key
+#  review_body :string(255)
+#  rating      :float
+#  helpfulness :integer
+#  flag        :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#  list_id     :integer
+#  user_id     :integer
+#
+
 require 'spec_helper'
 
 describe Review do
