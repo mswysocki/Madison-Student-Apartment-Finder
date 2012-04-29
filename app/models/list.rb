20490e683c7fa4528fef25e197c4fc4a5e9a29cd
Adam Eggum <aeggum12@gmail.com>
2012-04-29 13:41:40 -0500

    Added search by landlord to the search

--- app/models/list.rb
+++ app/models/list.rb
@@ -46,6 +46,8 @@ class List < ActiveRecord::Base
                   :gas,         :garbagecollection, :ltype, 
                   :length,      :furnished,         :laundry, 
                   :aptnum,      :building_name,     :landlord_id
+                  
+  attr_accessor   :landlord_name
   
   attr_searchable :address,     :city,              :state, 
                   :zip,         :region,            :bedrooms, 
@@ -90,8 +92,7 @@ class List < ActiveRecord::Base
                                   :presence => true
   end
   
-  
-  #sets default values for the db entry when the listing is initialized
+ 
   def default_values
     self.city ||= "Madison"
     self.state ||= "Wisconsin"
@@ -111,7 +112,6 @@ class List < ActiveRecord::Base
  
   ROOMS = (1..25).to_a
   ROOMS.insert(0, nil);
-  #BATHS = (1..10).to_a
   BATHS = [nil, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0]
   STREETS = {
       "street" => ["st",            "street"],   
