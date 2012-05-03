541db475d6d549caedff84b5ae29a0114a5b0a95
Adam Eggum <aeggum12@gmail.com>
2012-05-03 10:08:01 -0500

    Added many more Landlord controller tests

--- spec/controllers/landlords_controller_spec.rb
+++ spec/controllers/landlords_controller_spec.rb
@@ -32,17 +32,92 @@ describe LandlordsController do
   describe "GET 'show'" do
     before(:each) do
       @landlord = Factory(:landlord)
+      @listing1 = Factory(:list, :landlord_id => @landlord.id)
+      @listing2 = Factory(:list, :address => "New Address", :landlord_id => @landlord.id)
     end
   
     it "should be succesful" do
-      get :show, :id => @landlord.id
+      get :show, :id => @landlord
       response.should be_success
     end
     
     it "should have the right title" do
-      get :show, :id => @landlord.id
+      get :show, :id => @landlord
       response.should have_selector("title", :content => "Madison Student Housing Finder | #{@landlord.name}")
     end
+    
+    it "should contain the name of the landlord" do
+      get :show, :id => @landlord
+      response.should have_selectory("body", :content => @landlord.name)
+    end
+    
+    it "should show a table with listings in it" do
+      get :show, :id => @landlord
+      response.should have_selector("table")
+    end
+    
+    it "should show the first listing associated with the landlord" do
+      get :show, :id => @landlord
+      response.should have_selector("tr", :content => @listing1.name)
+    end
+    
+    it "should show the second listing associated with the landlord" do
+      get :show, :id => @landlord
+      response.should have_selector("tr", :content => @listing2.name)
+    end
   end
+  
+  
+  describe 'POST "create"' do
+    describe 'failure' do
+      before(:each) do
+        @attr = { :name => "", :email => "", :website => "", :city => "", :phone => "" }
+      end
+      
+      it "should not create a landlord" do
+        lambda do
+          post :create, :landlord => @attr
+        end.should_not change(Landlord, :count)
+      end
+      
+      it "should have the right title" do
+        post :create, :landlord => @attr
+        response.should have_selector("title", :content => "New Landlord")
+      end
+      
+      it "should render the 'new' page" do
+        post :create, :landlord => @attr
+        response.should render_template('new')
+      end
+    end
+    
+    describe 'success' do
+      before(:each) do
+        @attr = { :name => "New Landlord", :email => "a@abc.com", :phone => "6087903393",
+                      :city => "Madison", :zip => 53726 }
+      end
+      
+      it "should create a new landlord" do
+        lambda do
+          post :create, :landlord => @attr
+        end.should change(Landlord, :count).by(1)
+      end
+      
+      it "should redirect to the show page" do
+        post :create, :landlord => @attr
+        response.should redirect_to(landlord_path(assigns(:landlord)))
+      end
+    end
+  end
+
+
+
+
+
+
+
+
+
+
 
 end
