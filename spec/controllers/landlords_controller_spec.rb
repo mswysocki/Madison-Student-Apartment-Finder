4332ef3177531b70f6027c63b04692b516b18127
Adam Eggum <aeggum12@gmail.com>
2012-05-03 02:57:39 -0500

    Added some landlord controller tests.

--- spec/controllers/landlords_controller_spec.rb
+++ spec/controllers/landlords_controller_spec.rb
@@ -1,5 +1,48 @@
 require 'spec_helper'
 
 describe LandlordsController do
+  render views
+  
+  describe "GET 'new'" do 
+    it "should be successful" do
+      get 'new'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'new'
+      response.should have_selector("title", :content => "Madison Student Housing Finder | New landlord")
+    end
+  end
+  
+  
+  describe "GET 'index'" do
+    it "should be successful" do
+      get 'index'
+      response.should be_success
+    end 
+    
+    it "should have the right title" do
+      get :index
+      response.should have_selector("title", :content => "Madison Student Housing Finder | All landlords")
+    end
+  end
+  
+  
+  describe "GET 'show'" do
+    before(:each) do
+      @landlord = Factory(:landlord)
+    end
+  
+    it "should be succesful" do
+      get :show, :id => @landlord.id
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get :show, :id => @landlord.id
+      response.should have_selector("title", :content => "Madison Student Housing Finder | #{@landlord.name}")
+    end
+  end
 
 end
