34a3e2ccbcc29b0637824c4a680f213630849507
Adam Eggum <aeggum12@gmail.com>
2012-05-03 10:32:17 -0500

    Setting up for CarrierWave testing

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,3 +16,11 @@ CarrierWave.configure do |config|
   #config.fog_public     = false                                   # optional, defaults to true
   #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
+
+
+if Rails.env.test?
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+  end
+end
