b537582ab46698c68a2162e5f8ec662cab3d7a84
Adam Eggum <aeggum12@gmail.com>
2012-05-06 15:27:57 -0500

    Added deletion of landlords to the landlord controller.

--- app/controllers/landlords_controller.rb
+++ app/controllers/landlords_controller.rb
@@ -1,23 +1,27 @@
 class LandlordsController < ApplicationController
   
+  # GET /landlords/new
   def new
     session[:return_to] = request.referer
     @landlord = Landlord.new
     @title = "New landlord"
   end
   
+  # GET /landlords
   def index
     @title = "All landlords"
     @landlords = Landlord.paginate(:page => params[:page])
   end
   
+  
+  # GET /landlords/1
   def show
     @landlord = Landlord.find(params[:id])
     @title = @landlord.name
     @listings = get_listings(@landlord).paginate(:page => params[:page])
   end
   
-  
+  # POST /landlords
   def create
     @landlord = Landlord.new(params[:landlord])
     if @landlord.save
@@ -27,7 +31,15 @@ class LandlordsController < ApplicationController
       @title = "New Landlord"
       render 'new'
     end
-    
+  end
+  
+  # DELETE /landlords/1
+  def destroy 
+    session[:return_to] = request.referer
+    Landlord.find(params[:id]).destroy
+    #flash[:success] = "User destroyed."
+    #redirect_to users_path
+    redirect_to session[:return_to]
   end
   
   
