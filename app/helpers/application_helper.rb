2dfd2791c5d6391a34fd64b9f21d06014c40f0b3
Adam Eggum <aeggum12@gmail.com>
2012-05-07 00:03:38 -0500

    added some code to fix pagination with bootstrap.  Also did the admin page with some basics.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,4 +33,35 @@ module ApplicationHelper
   
   include Rack::Recaptcha::Helpers
   
+  class BootstrapLinkRenderer < ::WillPaginate::ActionView::LinkRenderer
+    protected
+
+    def html_container(html)
+      tag :div, tag(:ul, html), container_attributes
+    end
+
+    def page_number(page)
+      tag :li, link(page, page, :rel => rel_value(page)), :class => ('active' if page == current_page)
+    end
+
+    def gap
+      tag :li, link(super, '#'), :class => 'disabled'
+    end
+
+    def previous_or_next_page(page, text, classname)
+      tag :li, link(text, page || '#'), :class => [classname[0..3], classname, ('disabled' unless page)].join(' ')
+    end
+  end
+
+  def page_navigation_links(pages)
+    will_paginate(pages, :class => 'pagination', :inner_window => 2, :outer_window => 0, :renderer => BootstrapLinkRenderer, :previous_label => '&larr;'.html_safe, :next_label => '&rarr;'.html_safe)
+  end
+  
+  
+  def paginate *params
+    params[1] = {} if params[1].nil?
+    params[1][:renderer] = BootstrapPaginationHelper::LinkRenderer
+    will_paginate *params
+  end
+  
 end
